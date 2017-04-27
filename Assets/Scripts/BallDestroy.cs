7ce8839ef4062b1cb2b30134ef54d777f653bd28
MSI-Laptop\aleks <aleksander.niemiec@gmail.com>
2017-04-27 11:31:38 -0500

    Added walls and ball destroy script

---
+++ Assets/Scripts/BallDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDestroy : MonoBehaviour {
+
+    [Tooltip("Time it takes for ball to destroy itself after instantiation in seconds.")]
+    public float lifeTime = 5;
+
+	void Start () {
+        Destroy(gameObject, lifeTime);
+	}
+}
